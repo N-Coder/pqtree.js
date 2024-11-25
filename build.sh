8a2f608e46a1a44ee1a6dbbfeeeb7a953584b944
Simon D Fink <finksim@fim.uni-passau.de>
2024-11-25 08:54:11 +0100

    add GH actions/pages deploy workflow

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ set -e
 # source ../emsdk/emsdk_env.sh
 
 mkdir -p pctree-build
-emmake cmake -S pctree -B pctree-build -DCMAKE_BUILD_TYPE=Debug
-emmake make -C pctree-build
+emmake cmake -S pctree -B pctree-build -DCMAKE_BUILD_TYPE=Release
+emmake make -C pctree-build PCTree
 
 #mkdir -p pctreejs-build
-#emmake cmake -S . -B pctreejs-build -DCMAKE_BUILD_TYPE=Debug
+#emmake cmake -S . -B pctreejs-build -DCMAKE_BUILD_TYPE=Release
 #emmake make -C pctreejs-build
 
 emcc wasm/glue.cpp \
   pctree-build/libPCTree.a -I pctree/include \
   -s EXPORTED_FUNCTIONS='["_Process"]' -s EXPORTED_RUNTIME_METHODS='["cwrap"]' -s EXPORT_NAME="'PQTreeModule'" \
-  -s MODULARIZE -o wasm/libPCTree.js -O3
+  -s MODULARIZE -o static/libPCTree.js -O3
